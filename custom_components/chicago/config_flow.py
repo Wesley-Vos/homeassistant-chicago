0597110e1ca5ee5b6272c232a17ce8e5c884a0af
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2025-08-22 20:08:26 +0200

    Add reconfigure

--- custom_components/chicago/config_flow.py
+++ custom_components/chicago/config_flow.py
@@ -38,3 +38,20 @@ class ChicagoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA)
 
         return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+        
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Add reconfigure step to allow to manually reconfigure a config entry."""
+        reconfigure_entry = self._get_reconfigure_entry()
+        
+        if user_input is not None:
+            return self.async_update_reload_and_abort(
+                    reconfigure_entry,
+                data_updates=user_input
+                )
+  
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=CONFIG_SCHEMA
+        )
